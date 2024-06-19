0a9e9744d86dfef9ab819416435c0a57e60367ca
Desiderio Almansa Porrero <dalmansa@rfranco.es>
2024-06-19 18:19:00 +0200

    Created MongoDB and connected TuDuApp

--- TuDuApp_Back/src/index.ts
+++ TuDuApp_Back/src/index.ts
@@ -1,14 +1,14 @@
 import server from './server'
-import color from 'colors'
+import colors from 'colors'
 
 const port = process.env.Port || 5500
 
 server.listen(port, () => {
-    console.log(color.green.bold(`REST API running on port ${port}`));
+    console.log(colors.green.bold(`REST API running on port ${port}`));
   }).on('error', (err: NodeJS.ErrnoException) => {
     if (err.code === 'EADDRINUSE') {
-      console.error(color.red.bold(`Port ${port} is already in use. Please choose a different port.`));
+      console.error(colors.red.bold(`Port ${port} is already in use. Please choose a different port.`));
     } else {
-      console.error(color.red.bold(`Failed to start server: ${err.message}`));
+      console.error(colors.red.bold(`Failed to start server: ${err.message}`));
     }
   });
